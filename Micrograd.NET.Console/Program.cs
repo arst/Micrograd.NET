14646b7b6ea03a6f9e6e218be51a84edb61b4e5a
arst <artem.startsev@gmail.com>
2024-03-10 19:35:40 +0100

    Create Console project that uses the library

---
+++ Micrograd.NET.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Micrograd.NET;
+using Micrograd.NET.Trace;
+
+var a = new Value(3.0);
+var b = new Value(2.0);
+
+var c = a + b;
+
+await GraphTracer.RenderGraphToImage(c, "test.png");
+Console.WriteLine(c);
