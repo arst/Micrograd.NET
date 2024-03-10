18de5705496c3ab7c42a1a84f0594d0c43653437
arst <artem.startsev@gmail.com>
2024-03-10 20:08:49 +0100

    Use the same showcase example as original repsitory

--- Micrograd.NET.Console/Program.cs
+++ Micrograd.NET.Console/Program.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using Micrograd.NET;
-using Micrograd.NET.Trace;
 
-var a = new Value(3.0);
+var a = new Value(-4.0);
 var b = new Value(2.0);
-
 var c = a + b;
-
-await GraphTracer.RenderGraphToImage(c, "test.png");
-Console.WriteLine(c);
+var d = a * b + b.Pow(3);
+c += c + 1;
+c += 1 + c + (-a);
+d += d * 2 + (b + a).Relu();
+d += 3 * d + (b - a).Relu();
+var e = c - d;
+var f = e.Pow(2);
+var g = f / 2.0;
+g += 10.0 / f;
+Console.WriteLine($"{g.Data}"); // Expected to print: 24.7041
+g.Backprop();
+Console.WriteLine($"{a.Grad}"); // Expected to print: 138.8338
+Console.WriteLine($"{b.Grad}"); // Expected to print: 645.5773
