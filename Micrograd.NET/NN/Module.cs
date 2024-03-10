abb87e95d9dc66e053cf210ad2c0639df78d964f
arst <artem.startsev@gmail.com>
2024-03-10 19:34:21 +0100

    Create Module class

---
+++ Micrograd.NET/NN/Module.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Micrograd.NET.NN
+{
+    public abstract class Module
+    {
+        public void ZeroGrad()
+        {
+            foreach (var p in Parameters()) p.Grad = 0;
+        }
+
+        public virtual IEnumerable<Value> Parameters()
+        {
+            return Enumerable.Empty<Value>();
+        }
+    }
+}
