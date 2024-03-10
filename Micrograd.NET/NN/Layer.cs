a2b2c7360d0004a82540e93d37512bd4b5a7b43c
arst <artem.startsev@gmail.com>
2024-03-10 19:34:42 +0100

    Create Layer class

---
+++ Micrograd.NET/NN/Layer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Micrograd.NET.NN
+{
+    public class Layer : Module
+    {
+        private readonly List<Neuron> neurons;
+
+        public Layer(int nin, int nout, bool nonlin = true)
+        {
+            neurons = Enumerable.Range(0, nout).Select(_ => new Neuron(nin, nonlin)).ToList();
+        }
+
+        public IEnumerable<Value> Call(IEnumerable<Value> x)
+        {
+            var outList = neurons.Select(n => n.Call(x)).ToList();
+            return outList.Count == 1 ? new List<Value> { outList[0] } : outList;
+        }
+
+        public override IEnumerable<Value> Parameters()
+        {
+            return neurons.SelectMany(n => n.Parameters());
+        }
+
+        public override string ToString()
+        {
+            return $"Layer of [{string.Join(", ", neurons.Select(n => n.ToString()))}]";
+        }
+    }
+}
